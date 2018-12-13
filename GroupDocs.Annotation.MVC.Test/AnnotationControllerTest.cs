7ee305e960205f3b626633adf6b633f879805f89
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-12-13 14:59:46 +0200

    Added tests

---
+++ GroupDocs.Annotation.MVC.Test/AnnotationControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using GroupDocs.Annotation.MVC.Controllers;
+using NUnit.Framework;
+using System.Web.Routing;
+using MvcContrib.TestHelper;
+using Huygens;
+using System;
+
+namespace GroupDocs.Annotation.MVC.Test
+{
+    [TestFixture]
+    public class AnnotationControllerTest
+    {
+        
+        [SetUp]
+        public void TestInitialize()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            RouteTable.Routes.Clear();
+        }
+
+        [Test]
+        public void ViewStatusTest()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";            
+            using (var server = new DirectServer(path))
+            {
+                var request = new SerialisableRequest
+                {
+                    Method = "GET",
+                    RequestUri = "/annotation",
+                    Content = null
+                };
+
+                var result = server.DirectCall(request);
+                Assert.That(result.StatusCode, Is.EqualTo(200));
+            }
+        }
+
+        [Test]
+        public void ViewMapControllerTest()
+        {
+            "~/annotation".Route().ShouldMapTo<AnnotationController>(x => x.Index());
+        }
+    }
+}
