464248df5e132645df28d4d2595b573d54e6914e
pavelteplitsky <pavel.teplitsky@aspose.com>
2019-07-03 09:27:35 +0300

    Added support for forward slashies

--- src/Products/Annotation/Config/AnnotationConfiguration.cs
+++ src/Products/Annotation/Config/AnnotationConfiguration.cs
@@ -50,7 +50,7 @@ namespace GroupDocs.Annotation.MVC.Products.Annotation.Config
                     Directory.CreateDirectory(FilesDirectory);
                 }
             }
-            DefaultDocument = valuesGetter.GetStringPropertyValue("defaultDocument", DefaultDocument);
+            DefaultDocument = valuesGetter.GetStringPropertyValue("defaultDocument", DefaultDocument).Replace(@"\", "/");
             isTextAnnotation = valuesGetter.GetBooleanPropertyValue("textAnnotation", isTextAnnotation);
             isAreaAnnotation = valuesGetter.GetBooleanPropertyValue("areaAnnotation", isAreaAnnotation);
             isPointAnnotation = valuesGetter.GetBooleanPropertyValue("pointAnnotation", isPointAnnotation);
